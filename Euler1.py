37dd48e5177bfd5c78010638555c4ab2f4899afa
Reginald BIllings <237092991+ReggieB31@users.noreply.github.com>
2026-09-23 11:44:54 -0400

    First two solutions.

--- Euler1.py
+++ Euler1.py
@@ -1,1 +1,14 @@
-print("Hello World")
+## Find the sum of all multiples of 3 or 5 below 1000
+
+"""
+Loop over all numbers below 1000, check if each are divisible by 3 or 5.
+Continuosly append all numbers divisible by either to the sum.
+"""
+
+sum = 0
+
+for i in range(1000):
+    if (i % 5 == 0) or (i % 3 == 0):
+        sum += i
+
+print(sum)
---
+++ Euler2.py
@@ -0,0 +1,17 @@
+## By considering the terms in the Fibonacci sequence 
+# whose values do not exceed four million, find the sum of the even-valued terms.
+
+limit = 4000000
+lead, second = 1, 0
+total = 0
+while True:
+    num = lead + second
+    if num > limit:
+        break
+    if num % 2 == 0:
+        total += num
+    second = lead
+    lead = num
+    
+print(total)
+     
